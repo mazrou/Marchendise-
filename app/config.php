5e7161469c3679d7c41c36d0cabac15a3810fe70
Ayoub mazrou <ga_mazrou@esi.dz>
2020-04-04 23:11:18 +0100

    Update config.php

--- app/config.php
+++ app/config.php
@@ -10,7 +10,7 @@ define('VIEWS_PATH', APP_PATH . DS . 'view' . DS);
 
 defined('DATABASE_HOST_NAME')       ? null : define ('DATABASE_HOST_NAME', 'localhost');
 defined('DATABASE_USER_NAME')       ? null : define ('DATABASE_USER_NAME', 'root');
-defined('DATABASE_PASSWORD')        ? null : define ('DATABASE_PASSWORD', 'nitrou18b_boy');
+defined('DATABASE_PASSWORD')        ? null : define ('DATABASE_PASSWORD', '');
 defined('DATABASE_DB_NAME')         ? null : define ('DATABASE_DB_NAME', 'marchendise');
 defined('DATABASE_PORT_NUMBER')     ? null : define ('DATABASE_PORT_NUMBER', 3306);
 defined('DATABASE_CONN_DRIVER')     ? null : define ('DATABASE_CONN_DRIVER', 1);
